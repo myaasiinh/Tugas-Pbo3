42cc7c9fd5890b611acb1e505a41dc3b9e8f81ba
myaasiinh <myaasiinh.com>
2021-04-14 10:28:20 +0700

    fix and clean(warning masih ada sedikit error!!!)

--- src/com/tugaspbo3/Siswa.java
+++ src/com/tugaspbo3/Siswa.java
@@ -2,11 +2,6 @@ package com.tugaspbo3;
 
 public interface Siswa {
 
-    int nilai_mapel1 = 80;
-    int nilai_mapel2 = 95;
-    int nilai_mapel3 = 74;
-    int nilai_mapel4 = 65;
-
 
     void mapel1();
     void mapel2();
